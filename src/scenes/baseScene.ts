733c24aae04385badaebf568f3029297c9c3a6ef
Antti Lehtonen <antti.lehtonen@wunderdog.fi>
2022-09-11 15:53:11 +0300

    Now also obstacles and dog figure

--- src/scenes/baseScene.ts
+++ src/scenes/baseScene.ts
@@ -13,25 +13,26 @@ export class BaseScene extends Phaser.Scene {
   cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   spaceButton!: Phaser.Input.Keyboard.Key;
   player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-  obstacle!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   platforms!: Phaser.Physics.Arcade.StaticGroup;
+  obstacles!: Phaser.Types.Physics.Arcade.ImageWithStaticBody[];
   movingPlatform!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
   dangerous!: Phaser.Physics.Arcade.Group;
   isJumping = false;
   sceneIndex!: number;
   roomIndex: number = 0;
+  playerGroundCollider!: Phaser.Physics.Arcade.Collider;
+  music!: Phaser.Sound.BaseSound;
 
   constructor({ sceneIndex }: Props) {
     super({ active: false, visible: false });
     this.sceneIndex = sceneIndex;
-    this.roomIndex = 1;
+    this.roomIndex = 0;
     const key = `GameScene${sceneIndex}`;
     Phaser.Scene.call(this, { key });
   }
 
   init(data: InitParams) {
     if (data && data.roomIndex) {
-      console.log('room', data.roomIndex)
       this.roomIndex = data.roomIndex;
     }
   }
@@ -43,24 +44,33 @@ export class BaseScene extends Phaser.Scene {
       frameWidth: 32,
       frameHeight: 48
     });
-    this.load.image('rock', 'assets/images/tile_0027.png');
+    this.load.image('rock', 'assets/images/rock.png');
+    this.load.spritesheet('dog', 'assets/images/dog.png', {
+      margin: 0,
+      spacing: 0,
+      frameWidth: 32,
+      frameHeight: 32
+    });
+    this.load.image('vertical_obstacle', 'assets/images/vertical_obstacle.png');
+
+    this.load.audio('gameover', ['assets/music/fallingdown.mp3']);
   }
 
   createAnims() {
     this.anims.create({
       key: 'left',
-      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frames: this.anims.generateFrameNumbers('dog', { start: 52, end: 55 }),
       frameRate: 10,
       repeat: -1
     });
     this.anims.create({
       key: 'turn',
-      frames: [{ key: 'dude', frame: 4 }],
+      frames: [{ key: 'dog', frame: 36 }],
       frameRate: 20
     });
     this.anims.create({
       key: 'right',
-      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frames: this.anims.generateFrameNumbers('dog', { start: 5, end: 8 }),
       frameRate: 10,
       repeat: -1
     });
@@ -73,8 +83,15 @@ export class BaseScene extends Phaser.Scene {
     );
   }
 
+  createMusic() {
+    this.music = this.game.sound.add('gameover', {
+      loop: false,
+      volume: 5
+    });
+  }
+
   createDangerous() {
-    const roomIndex = this.roomIndex;
+    const roomIndex = this.roomIndex; // roomIndex = 3;
     if (roomIndex === 0) {
       this.dangerous = this.physics.add.group({
         key: 'rock',
@@ -86,17 +103,49 @@ export class BaseScene extends Phaser.Scene {
         child.body.gameObject.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
         child.body.gameObject.setVelocityX(0);
       });
-    } else if (roomIndex < 5) {
+    } else if (roomIndex === 1) {
       this.dangerous = this.physics.add.group({
         key: 'rock',
         repeat: 2,
-        setXY: { x: END_X/2, y: END_Y, stepX: 300 }
+        setXY: { x: END_X / 2, y: END_Y, stepX: 300 }
       });
 
-        this.dangerous.children.iterate((child) => {
-          child.body.gameObject.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-          child.body.gameObject.setVelocityX(-100 - 10 * roomIndex);
-        });
+      this.dangerous.children.iterate((child) => {
+        child.body.gameObject.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        child.body.gameObject.setVelocityX(-100 - 10 * roomIndex);
+      });
+    } else if (roomIndex === 2) {
+      const offset = 200;
+      this.obstacles = new Array(3)
+        .fill(0)
+        .map((item, index) =>
+          this.physics.add.staticImage(
+            300 + index * offset,
+            220,
+            'vertical_obstacle'
+          )
+        );
+    } else if (roomIndex === 3) {
+      const offset = 200;
+      this.obstacles = new Array(3)
+        .fill(0)
+        .map((item, index) =>
+          this.physics.add.staticImage(
+            300 + index * offset,
+            220,
+            'vertical_obstacle'
+          )
+        );
+      this.dangerous = this.physics.add.group({
+        key: 'rock',
+        repeat: 0,
+        setXY: { x: END_X / 2, y: END_Y, stepX: 300 }
+      });
+
+      this.dangerous.children.iterate((child) => {
+        child.body.gameObject.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        child.body.gameObject.setVelocityX(-100 - 10 * roomIndex);
+      });
     }
   }
 
@@ -104,25 +153,33 @@ export class BaseScene extends Phaser.Scene {
     this.add.image(400, 300, 'sky');
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-    this.player = this.physics.add.sprite(100, 450, 'dude').setScale(1.5);
+    this.player = this.physics.add.sprite(100, 450, 'dog').setScale(1.5);
     this.player.setBounce(0.2);
     this.player.setCollideWorldBounds(true);
 
     this.createAnims();
     this.createKeys();
     this.createDangerous();
+    this.createMusic();
 
-    this.physics.add.collider(this.dangerous, this.platforms);
-    this.physics.add.collider(this.player, this.platforms);
-
-    this.physics.add.overlap(this.player, this.dangerous, (player, rock) =>
-      this.hitTheRock()
+    this.playerGroundCollider = this.physics.add.collider(
+      this.player,
+      this.platforms
     );
+    if (this.obstacles) {
+      this.physics.add.collider(this.player, this.obstacles);
+    }
+    if (this.dangerous && this.dangerous.children) {
+      this.physics.add.collider(this.dangerous, this.platforms);
+      this.physics.add.overlap(this.player, this.dangerous, (player, rock) =>
+        this.hitTheRock()
+      );
+    }
   }
 
   update() {
     const { cursors, player, movingPlatform, spaceButton } = this;
-    if (true /* !this.isJumping */) {
+    if (!this.isJumping) {
       if (cursors.left.isDown) {
         player.setVelocityX(-160);
         player.anims.play('left', true);
@@ -141,13 +198,19 @@ export class BaseScene extends Phaser.Scene {
       player.setVelocityY(-330);
       this.isJumping = true;
     }
-    this.dangerous.children.iterate((child) => {
-      if (child.body.position.x < 10) {
-        child.body.position.x = END_X;
-      }
-    });
+    if (this.dangerous && this.dangerous.children) {
+      this.dangerous.children.iterate((child) => {
+        if (child.body.position.x < 10) {
+          child.body.position.x = END_X;
+        }
+      });
+    }
     if (player.x >= END_X) {
       this.nextScene();
+    }
+    if (player.y >= END_Y + 100) {
+      // fallen too much
+      this.restartScene();
     }
   }
 
@@ -162,6 +225,12 @@ export class BaseScene extends Phaser.Scene {
   }
 
   hitTheRock() {
-    this.restartScene();
+    this.player.setVelocityX(0);
+    this.player.setVelocityY(50);
+    this.player.body.allowGravity = false;
+    this.physics.world.removeCollider(this.playerGroundCollider);
+    this.player.setCollideWorldBounds(false);
+    // this.game.sound.resumeAll();
+    // this.music.play();
   }
 }
